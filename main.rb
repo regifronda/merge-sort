84e8c623598085f845ffc2c061e92f0ff70e48b9
Regi Fronda <regisfronda@gmail.com>
2021-07-13 15:14:55 -0700

    Add method merge method

--- main.rb
+++ main.rb
@@ -8,11 +8,23 @@
     # combine left and right half of the arrays
 
 def merge_sort(array)
-  length = array.length
-  
-  if length < 2
+  if array.length <= 1
     return array
+  else
+    midpoint = array.length/2
+    left_half = merge_sort(array.slice(0...midpoint))
+    right_half = merge_sort(array.slice(midpoint...array.length))
+    merge(left_half, right_half)
   end
 end
 
-p merge_sort([3])
+def merge(left_half, right_half)
+  sorted_array = []
+  while !left_half.empty? && !right_half.empty? do
+    if left_half[0] < right_half[0]
+      sorted_array.push(left_half.shift)
+    else
+      sorted_array.push(right_half.shift)
+    end
+  end
+end

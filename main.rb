e7b49fc3fa2454352fefe66d30e2a8767cf1a0b8
Regi Fronda <regisfronda@gmail.com>
2021-07-13 04:51:11 -0700

    Add pseudocode for merge sort method

---
+++ main.rb
@@ -0,0 +1,8 @@
+# merge_sort method that accepts an array as an argument
+  # find the length of the array
+  # if length < 2
+    #return
+  # else
+    # sort left half of array
+    # sort right half of array
+    # combine left and right half of the arrays 

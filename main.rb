72033d60d38770384a9ee0a86ae81ce0bdf9ae18
Regi Fronda <regisfronda@gmail.com>
2021-07-13 15:16:07 -0700

    Remove comments

--- main.rb
+++ main.rb
@@ -1,12 +1,3 @@
-# merge_sort method that accepts an array as an argument
-  # find the length of the array
-  # if length < 2
-    #return
-  # else
-    # sort left half of array
-    # sort right half of array
-    # combine left and right half of the arrays
-
 def merge_sort(array)
   if array.length <= 1
     return array
